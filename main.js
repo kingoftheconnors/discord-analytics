c7bbf18f58c5d9d9525986ef19ede1d8b83d3011
kingoftheconnors <Connormcp777@gmail.com>
2019-01-26 00:32:14 -0500

    Log times

--- main.js
+++ main.js
@@ -1,15 +1,47 @@
 #!/usr/bin/env node
 
-require('dotenv').config();
-const Discord = require('discord.js');
-const client = new Discord.Client();
+require('dotenv').config()
+const Discord = require('discord.js')
+const moment = require('moment')
+const client = new Discord.Client()
+var userTimes = {}
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
 client.on('presenceUpdate', msg => {
-  console.log(`${msg.user.username}#${msg.user.discriminator}: ${msg.user.presence.status}`);
+  console.log(`${msg.user.username}#${msg.user.discriminator}: ${msg.user.presence.status}`)
+  if (!userTimes[msg.user.username]) {
+    userTimes[msg.user.username] = []
+  }
+
+  var importantStuff = {
+    username: `${msg.user.username}#${msg.user.discriminator}`,
+    status: msg.user.presence.status,
+    timestamp: moment()
+  }
+
+  userTimes[msg.user.username].push(importantStuff)
 });
 
-client.login(process.env.BOT_TOKEN);
+client.on('message', msg => {
+  if (msg.content === 'stats') {
+    console.log(userTimes)
+    // msg.reply(formatTimes(), {reply: null})
+    msg.channel.send(formatTimes())
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
+
+function formatTimes() {
+  var retVal = ""
+  Object.keys(userTimes).forEach( (username) =>{
+    userTimes[username].forEach( (event) => {
+      retVal += `${event.username}: ${event.status} at ${event.timestamp.format()}\n`
+    })
+  })
+  
+  return retVal
+}
