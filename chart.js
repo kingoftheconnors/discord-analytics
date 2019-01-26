8216914569b5e753cfa6eac49c8721728648b7b9
Tom Richards <tom@tomrichards.net>
2019-01-26 01:00:19 -0600

    Use highcharts

--- chart.js
+++ chart.js
@@ -1,39 +1,39 @@
-// require file system and jsdom
-var fs = require('fs');
+// Load Highcharts
+const exporter = require('highcharts-export-server')
+const fs = require('fs')
+ 
+//Export settings
+var exportSettings = {
+  type: 'png',
+  options: {
+      title: {
+          text: 'My Chart'
+      },
+      xAxis: {
+          categories: ["Jan", "Feb", "Mar", "Apr", "Mar", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+      },
+      series: [
+          {
+              type: 'line',
+              data: [1, 3, 2, 4]
+          },
+          {
+              type: 'line',
+              data: [5, 3, 4, 2]
+          }
+      ]
+  }
+};
+ 
+//Set up a pool of PhantomJS workers
+exporter.initPool();
 
-// For jsdom version 10 or higher.
-// Require JSDOM Class.
-var JSDOM = require('jsdom').JSDOM;
-// Create instance of JSDOM.
-var jsdom = new JSDOM('<body><div id="container"></div></body>', {runScripts: 'dangerously'});
-// Get window
-var window = jsdom.window;
+//Perform an export
+exporter.export(exportSettings, function (err, res) {
+    var outData = new Buffer(res.data, 'base64')
+    fs.writeFileSync('out.png', outData)
 
-// For jsdom version 9 or lower
-// var jsdom = require('jsdom').jsdom;
-// var document = jsdom('<body><div id="container"></div></body>');
-// var window = document.defaultView;
-
-// require anychart and anychart export modules
-var anychart = require('anychart')(window);
-var anychartExport = require('anychart-nodejs')(anychart);
-
-// create and a chart to the jsdom window.
-// chart creating should be called only right after anychart-nodejs module requiring
-var chart = anychart.pie([10, 20, 7, 18, 30]);
-chart.bounds(0, 0, 800, 600);
-chart.container('container');
-chart.draw();
-
-// generate JPG image and save it to a file
-anychartExport.exportTo(chart, 'jpg').then(function(image) {
-  fs.writeFile('anychart.jpg', image, function(fsWriteError) {
-    if (fsWriteError) {
-      console.log(fsWriteError);
-    } else {
-      console.log('Complete');
-    }
-  });
-}, function(generationError) {
-  console.log(generationError);
+    //Kill the pool when we're done with it, and exit the application
+    exporter.killPool();
+    process.exit(1);
 });
